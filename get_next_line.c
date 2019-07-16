579c68115e3b0067689bc5e6a7d579e55a7879fc
Liam Du Preez <ldu-pree@c3r8s7.wethinkcode.co.za>
2019-07-16 12:56:27 +0200

    added comments to code

--- get_next_line.c
+++ get_next_line.c
@@ -6,78 +6,79 @@
 /*   By: ldu-pree <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 09:44:52 by ldu-pree          #+#    #+#             */
-/*   Updated: 2019/06/28 09:45:00 by ldu-pree         ###   ########.fr       */
+/*   Updated: 2019/07/16 12:55:11 by ldu-pree         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static	int			gnl_verify_line(char **stack, char **line)
+int		ft_new_line(char **s, char **line, int fd, int ret)
 {
-	char			*tmp_stack;
-	char			*strchr_stack;
-	int				i;
+	char	*tmp;
+	int		len;
 
-	i = 0;
-	strchr_stack = *stack;
-	while (strchr_stack[i] != '\n')
-		if (!strchr_stack[i++])
-			return (0);
-	tmp_stack = &strchr_stack[i];
-	*tmp_stack = '\0';
-	*line = ft_strdup(*stack);
-	*stack = ft_strdup(tmp_stack + 1);
+	len = 0;
+	//checks the length of the line so it stops when it either reaches a newline character or a null character
+	while (s[fd][len] != '\n' && s[fd][len] != '\0')
+		len++;
+	if (s[fd][len] == '\n')
+	{
+		//sets line equal to the string of s[fd] that startsat 0 and ends at len
+		*line = ft_strsub(s[fd], 0, len);
+		//allocates memory for a string of size s[fd] and len and 1 byte extra for null and copies from s[fd] to tmp
+		tmp = ft_strdup(s[fd] + len + 1);
+		//frees s[fd] to prevent leaks
+		free(s[fd]);
+		//sets s[fd] to tmp the result of the copy
+		s[fd] = tmp;
+		//now checks if the start of the string is equal to null if so then deletes the string
+		if (s[fd][0] == '\0')
+			ft_strdel(&s[fd]);
+	}
+	else if (s[fd][len] == '\0')
+	{
+		if (ret == BUFF_SIZE)
+			return (get_next_line(fd, line));
+		*line = ft_strdup(s[fd]);
+		ft_strdel(&s[fd]);
+	}
 	return (1);
 }
 
-static	int			gnl_read_file(int fd, char *heap, char **stack, char **line)
+int		get_next_line(const int fd, char **line)
 {
-	int				ret;
-	char			*tmp_stack;
+	static char	*s[255];
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
+	int			ret;
 
-	while ((ret = read(fd, heap, BUFF_SIZE)) > 0)
+	//ret is the amount of bytes the function reads from filedescriptor
+	/*checking for valid inputs*/
+	if (fd < 0 || line == NULL)
+		return (-1);
+	/*while the bytes it reads from the file of fd into buf is bigger than 0*/
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		heap[ret] = '\0';
-		if (*stack)
-		{
-			tmp_stack = *stack;
-			*stack = ft_strjoin(tmp_stack, heap);
-			free(tmp_stack);
-			tmp_stack = NULL;
-		}
-		else
-			*stack = ft_strdup(heap);
-		if (gnl_verify_line(stack, line))
+		//final character is null since it is a string it needs to end with null
+		buf[ret] = '\0';
+		//this creates a string that wecan read from andwrite toaswell as return
+		if (s[fd] == NULL)
+			//allocates it 2 bytes of memory
+			s[fd] = ft_strnew(1);
+		tmp = ft_strjoin(s[fd], buf);
+		//frees s to prevent any leaks
+		free(s[fd]);
+		s[fd] = tmp;
+		//searches for a newline character in buf and once found it breaks this loop
+		if (ft_strchr(buf, '\n'))
 			break ;
 	}
-	return (RET_VALUE(ret));
-}
-
-int					get_next_line(int const fd, char **line)
-{
-	static char		*stack[MAX_FD];
-	char			*heap;
-	int				ret;
-	int				i;
-
-	if (!line || (fd < 0 || fd >= MAX_FD) || (read(fd, stack[fd], 0) < 0) \
-		|| !(heap = (char *)malloc(sizeof(char) * BUFF_SIZE + 1)))
+	//now returns -1 if ret is smaller than 0 since there would then be no characters in the line
+	if (ret < 0)
 		return (-1);
-	if (stack[fd])
-		if (gnl_verify_line(&stack[fd], line))
-			return (1);
-	i = 0;
-	while (i < BUFF_SIZE)
-		heap[i++] = '\0';
-	ret = gnl_read_file(fd, heap, &stack[fd], line);
-	free(heap);
-	if (ret != 0 || stack[fd] == NULL || stack[fd][0] == '\0')
-	{
-		if (!ret && *line)
-			*line = NULL;
-		return (ret);
-	}
-	*line = stack[fd];
-	stack[fd] = NULL;
-	return (1);
+	//or 0 if there is only a null character
+	else if (ret == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
+		return (0);
+	//finally checks the line
+	return (ft_new_line(s, line, fd, ret));
 }
